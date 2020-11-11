ec184f8c8ad59feaed1a3f8e839a0dc9240f6459
Parada <paradafaii28@gmail.com>
2020-11-11 14:11:12 -0700

    style header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import './Header.css'
 
 export default function Header() {
     return (
         <div className="heading">
-            <h1>My todo app</h1>
+            <h1>My TODO <span className="italic secondary-color">App</span></h1>
         </div>
     )
 }
